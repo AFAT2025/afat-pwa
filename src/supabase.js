3953f23e54dbb7940f79979d4faaba34854cc9fe
AFAT2025 <afatambato@gmail.com>
2026-05-17 16:21:45 +0000

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'COLA_AQUI_O_PROJECT_URL_DO_PASSO_4'
-const SUPABASE_KEY = 'COLA_AQUI_A_ANON_KEY_DO_PASSO_4'
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
